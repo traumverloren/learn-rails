c5b10ae93a14fd7d2f8fb2e3ff7f2b7facc73fb4
Stephanie Nemeth <traumverloren@gmail.com>
2014-12-19 17:00:35 +0100

    front-end framework, add foundation styling and simple_forms

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,8 +2,6 @@ class VisitorsController < ApplicationController
 
   def new
     @owner = Owner.new
-    flash.now[:notice] = 'welcome!'
-    flash.now[:alert] = 'my birthday is soon!'
   end
 
 end
